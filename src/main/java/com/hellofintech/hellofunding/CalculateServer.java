8eb8a463f3fbabf13450ff2f1a22b5e3d847046f
yoonsunmi <43695910+yoonsunmi@users.noreply.github.com>
2025-02-17 17:50:36 +0900

    Feat: add basic setting

---
+++ src/main/java/com/hellofintech/hellofunding/CalculateServer.java
@@ -0,0 +1,11 @@
+package com.hellofintech.hellofunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateServer {
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateServer.class, args);
+	}
+}
